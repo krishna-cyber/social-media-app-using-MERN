0267fd977358f7e9096f26622c97d8e9b752017a
krishna tiwari <tiwarikrishna54321@gmail.com>
2022-11-07 21:48:42 +0545

    component created at left for tommorrow

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
+import {
+  Typography,
+  Grid,
+  Container,
+  AppBar,
+  Grow,
+  Toolbar,
+} from "@material-ui/core";
+
 function App() {
   return (
     <>
-      <h1>Hello everyone</h1>
+      <Container maxWidth='lg'>
+        <AppBar position='static' color='primary'>
+          <Toolbar>
+            <Typography variant='h2' align='center'>
+              Memories
+            </Typography>
+            <img src={memories} alt='memories' height={60} />
+          </Toolbar>
+        </AppBar>
+      </Container>
     </>
   );
 }
