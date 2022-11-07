eeb33c979028b06ac0f2623fa5a16acf424ac4e1
krishna tiwari <tiwarikrishna54321@gmail.com>
2022-11-07 20:53:27 +0545

    server created and databse connected successfully

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+// require("./models/dbConnection");
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+//server start and database connection start
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `Server is running on port ${process.env.PORT} and database is connected`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
