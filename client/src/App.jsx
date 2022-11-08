2503a37cb7e1ba0c2bfe61758d46c549428cd8c2
krishna tiwari <tiwarikrishna54321@gmail.com>
2022-11-08 12:34:36 +0545

    simple design basics created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,18 +7,43 @@ import {
   Toolbar,
 } from "@material-ui/core";
 
+import Form from "./components/form/Form";
+import Posts from "./components/posts/Posts";
+import memories from "./images/memories.png";
+import useStyles from "./styles";
+
 function App() {
+  const classes = useStyles();
   return (
     <>
       <Container maxWidth='lg'>
-        <AppBar position='static' color='primary'>
-          <Toolbar>
-            <Typography variant='h2' align='center'>
-              Memories
-            </Typography>
-            <img src={memories} alt='memories' height={60} />
-          </Toolbar>
+        <AppBar className={classes.appbar} position='static' color='inherit'>
+          <Typography className={classes.heading} variant='h2' align='center'>
+            Memories
+          </Typography>
+          <img
+            className={classes.image}
+            src={memories}
+            alt='memories'
+            height='60'
+          />
         </AppBar>
+        <Grow in>
+          <Container>
+            <Grid
+              container
+              justifyContent='space-between'
+              alignItems='stretch'
+              spacing={3}>
+              <Grid item xs={12} sm={7}>
+                <Posts />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form />
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
       </Container>
     </>
   );
